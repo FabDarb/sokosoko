63c35a7da3c7b93b1c57ecd6e26c2c268a8e0362
Fabien Darbellay <Fabien.Darbellay@ceff.ch>
2023-07-24 19:55:45 +0200

    travaille sur la délimitation des niveaux

--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -26,6 +26,7 @@ namespace sokosoko
         public int test = 1;
         public string lvl = "1 First steps - Beginner_0.txt";
         public string chois = "1 First steps - Beginner";
+        public int num = 99;
         public Window1()
         {
             InitializeComponent();
@@ -39,79 +40,74 @@ namespace sokosoko
         {
             chois = "1 First steps - Beginner";
             LblChoix.Content = "Facile";
-            
+            num = 99;
+            check();
+            test = 1;
+            lblMax.Content = "99";
         }
 
         private void ramoyen_Checked(object sender, RoutedEventArgs e)
         {
             chois = "2 First steps - Advanced";
             LblChoix.Content = "moyen";
-            
+            num = 79;
+            check();
+            test = 2;
+            lblMax.Content = "79";
         }
 
         private void radifficile_Checked(object sender, RoutedEventArgs e)
         {
             chois = "3 First steps - Expert";
             LblChoix.Content = "difficile";
-            
+            num = 29;
+            check();
+            test = 3;
+            lblMax.Content = "29";
         }
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
             numMonde = int.Parse(txtNb.Text);
-            if (chois == "1 First steps - Beginner")
-            {
-                if (numMonde > 99 || numMonde < 0)
-                {
-                    numMonde = 0;
-                    test = 1;
-                }
-                else
-                {
-                    test = 1;
-                    //lvl = $"1 First steps - Beginner_{numMonde}.txt";
-                }
-            }
-            else if(chois == "2 First steps - Advanced")
-            {
-                if (numMonde > 79 || numMonde < 0)
-                {
-                    numMonde = 0;
-                    test = 2;
-                }
-                else
-                {
-                    test = 2;
-                    //lvl = $"2 First steps - Advanced_{numMonde}.txt";
-                }
-            }
-            else
-            {
-                if (numMonde > 29 || numMonde < 0)
-                {
-                    numMonde = 0;
-                    test = 3;
-                }
-                else
-                {
-                    test = 3;
-                    //lvl = $"3 First steps -Expert_{numMonde}.txt";
-                }
-            }
+            
 
             this.Close();
         }
 
         private void Btn_Plus_Click(object sender, RoutedEventArgs e)
         {
+            numMonde = int.Parse(txtNb.Text);
             numMonde++;
+            if(numMonde > num)
+            {
+                numMonde--;
+            }
             txtNb.Text = numMonde.ToString();
         }
 
         private void Btn_moin_Click(object sender, RoutedEventArgs e)
         {
+            numMonde = int.Parse(txtNb.Text);
             numMonde--;
+            if (numMonde < 0)
+            {
+                numMonde++;
+            }
             txtNb.Text = numMonde.ToString();
+        }
+
+        void check()
+        {
+            btnAdd.IsEnabled = true;
+            if (numMonde > num || numMonde < 0)
+            {
+                btnAdd.IsEnabled = false;
+            }
+        }
+
+        private void txtNb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            check();
         }
     }
 }
